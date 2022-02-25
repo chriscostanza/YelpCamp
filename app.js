b46ad8516cf563d20992dbc5016ffd3d9e641d73
Chris Costanza <christopherjohncostanza@gmail.com>
2022-02-24 21:57:57 -0500

    second commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const res = require('express/lib/response');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000")
+})
