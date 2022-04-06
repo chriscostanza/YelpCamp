5f34007a498051219365f429128ed78a7165cbc2
Chris Costanza <christopherjohncostanza@gmail.com>
2022-04-06 12:41:33 -0400

    procfile

--- app.js
+++ app.js
@@ -177,6 +177,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(process.env.PORT || 3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log(`LISTENING ON PORT ${port}`)
 });
