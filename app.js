fd96564a33dd43ad4a97ed6f876abbcdade4aed0
Chris Costanza <christopherjohncostanza@gmail.com>
2022-04-06 12:24:41 -0400

    fixed port issue

--- app.js
+++ app.js
@@ -177,7 +177,6 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`LISTENING ON PORT ${port}`)
 });
