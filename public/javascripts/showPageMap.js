6d8c807a95d6439c55f6b5fe11c1185f09ff4090
Chris Costanza <christopherjohncostanza@gmail.com>
2022-04-06 15:35:49 -0400

    made more responsive

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -9,11 +9,11 @@ const map = new mapboxgl.Map({
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
     .setPopup(
-        new mapboxgl.Popup({offset: 25})
-        .setHTML(
-            `<h3>${campground.title}</h3><p>${campground.location}</p>`
-        )
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
+            )
     )
     .addTo(map)
 
-    map.addControl(new mapboxgl.NavigationControl());
+map.addControl(new mapboxgl.NavigationControl());
